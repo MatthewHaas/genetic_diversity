79099c0795a14689aa7c11a15d446fa0dd9c2a6c
Matthew Haas <matthew.haas1@gmail.com>
2019-07-05 16:30:48 -0500

    Update to file
    Added other large scaffolds (15) representing ~90% of the Zizania palustris genome.

--- pca_from_vcf.R
+++ pca_from_vcf.R
@@ -1,8 +1,14 @@
 # 1 July 2019
 # Making a PCA plot from vcf files
 
-# Merge multiple vcf files into one?
-bcftools concat '190627_samtools_Scaffold_48;HRSCAF=1451.vcf.gz' '190627_samtools_Scaffold_70;HRSCAF=1591.vcf.gz' > merged_file.vcf.gz
+# Merge multiple vcf files into one
+# module load bcftools
+bcftools concat -o seventeen_largest_vcfs_merged.vcf.gz '190627_samtools_Scaffold_48;HRSCAF=1451.vcf.gz' '190627_samtools_Scaffold_70;HRSCAF=1591.vcf.gz' '190627_samtools_Scaffold_1064;HRSCAF=3428.vcf.gz'
+'190627_samtools_Scaffold_415;HRSCAF=2522.vcf.gz' '190627_samtools_Scaffold_93;HRSCAF=1687.vcf.gz' '190627_samtools_Scaffold_18;HRSCAF=1069.vcf.gz'
+'190627_samtools_Scaffold_13;HRSCAF=912.vcf.gz' '190627_samtools_Scaffold_1;HRSCAF=83.vcf.gz' '190627_samtools_Scaffold_1062;HRSCAF=3426.vcf.gz'
+'190627_samtools_Scaffold_1065;HRSCAF=3429.vcf.gz' '190627_samtools_Scaffold_9;HRSCAF=810.vcf.gz' '190627_samtools_Scaffold_3;HRSCAF=502.vcf.gz'
+'190627_samtools_Scaffold_1063;HRSCAF=3427.vcf.gz' '190627_samtools_Scaffold_7;HRSCAF=722.vcf.gz' '190627_samtools_Scaffold_693;HRSCAF=2950.vcf.gz'
+'190627_samtools_Scaffold_51;HRSCAF=1459.vcf.gz'
 
 # Load required packages
 library(vcfR)
@@ -13,7 +19,7 @@ library(data.table)
 
 # Read in data
 # For now this is only the biggest scaffold (test run), but should use all (or at least the biggest) to get a better idea of structure
-x <- read.vcfR("merged_file.vcf.gz")
+x <- read.vcfR("seventeen_largest_vcfs_merged.vcf.gz")
 
 # Convert to a genlight object (for pooppr and adegenet)
 gen_light_x <- vcfR2genlight(x)
@@ -23,7 +29,7 @@ dist <- dist(gen_light_x)
 
 # Principal component analysis
 # This step took a long time--adding cores may help
-pca <- glPca(gen_light_x, nf=8, n.cores=2)
+pca <- glPca(gen_light_x, nf=8, n.cores=10)
 
 # Make into data.table
 pca_scores <- as.data.table(pca$scores)
@@ -44,8 +50,8 @@ pca_scores[, col := c(1:7)]
 
 # Make the PCA plot
 # signif() digits are different so that they will be the same in the plot (to the hundreths place)--likely due to % variance for PC2 being less than 1
-pdf("190702_pilot_GBS_PCA.pdf")
-pca_scores[, plot(PC1, PC2, xlab=paste0("PC1: ", signif(percentVar[1]*100, digits=3), "%"), ylab=paste0("PC2: ", signif(percentVar[2]*100, digits=3), "%"), main=paste0("PCA for pilot GBS", "\n", "Scaffold 48"), yaxt='n', pch=16, col=col)]
+pdf("190705_pilot_GBS_PCA.pdf")
+pca_scores[, plot(PC1, PC2, xlab=paste0("PC1: ", signif(percentVar[1]*100, digits=3), "%"), ylab=paste0("PC2: ", signif(percentVar[2]*100, digits=3), "%"), main=paste0("PCA for pilot GBS", "\n", "17 largest scaffolds"), yaxt='n', pch=16, col=col)]
 axis(2, las=2)
 legend("topright", legen=c("14S-PS", "Garfield Lake", "Itasca-C12", "Latifolia", "Necktie Lake", "PM3E", "Upper Rice Lake"), col=c(1:7), pch=16, bty='n')
 dev.off()
