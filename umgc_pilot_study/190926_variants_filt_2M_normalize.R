77fa1a6db5f3e60ba55610c58ea9a100d6f66a6a
Matthew Haas <matthew.haas1@gmail.com>
2019-09-26 21:29:39 -0500

    Updated file
    Updated the file to convert the data from the TSV file (filtered, 2 million depth) that was saved as an R object in long format to wide format. After conversion to wide format, it was saved as a CSV file for export. Next step: do the same process fo all other filtered variant depths (0.5M, 1M, 4M, 7M).

--- umgc_pilot_study/190926_variants_filt_2M_normalize.R
+++ umgc_pilot_study/190926_variants_filt_2M_normalize.R
@@ -1,8 +1,9 @@
 # 26 September 2019
 # WD: /home/jkimball/haasx092/umgc_pilot_study
 
-# Load required package
+# Load required packages
 library(data.table)
+library(reshape2)
 
 # Read in data (2 million read depth)
 fread("190926_variants_filt_2M_normalize.tsv") -> x
@@ -12,3 +13,18 @@ setnames(x, c("scaffold", "pos", "ref", "alt", "quality", "sample", "GT", "V8", 
 
 # Save data as an R data file 
 save(x, file="190926_variants_filt_2M_normalize.Rdata")
+
+# Convert from long format to wide format
+y <- dcast(x, scaffold + pos ~ sample, value.var="GT")
+
+# Convert back to data table format (using genotype "GT" as the variable value (vs ref or alt allele))
+y <- as.data.table(y)
+
+# Change colnames to fix accession names. I'm mostly interested in fixing NecktieLake (it is actually a river) but I am changing periods in accession names back to hyphens (although this is probably unnecessary since it doesn't change the meaning of the name)
+setnames(y, c("scaffold", "pos", "14S-PS", "FY-C20", "GarfieldLake", "Itasca-C12", "NecktieRiver", "PM3E", "UpperRiceLake"))
+
+# Create a csv file for export that can be used in downstream analyses and eventually deposited in a repository such as e!DAL
+write.csv(y, file="190926_variants_filt_2M_normalize.csv", col.names=TRUE, row.names=FALSE)
+
+# (Re)save the updated objects
+save(x, y, file="190926_variants_filt_2M_normalize.Rdata")
