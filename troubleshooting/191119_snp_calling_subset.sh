2f94c9b6e1fae2084c9d44d2f8125d9ba46bb914
Matthew Haas <matthew.haas1@gmail.com>
2019-11-19 21:22:39 -0600

    Initial commit

---
+++ troubleshooting/191119_snp_calling_subset.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l 
+#PBS -l nodes=1:ppn=8,mem=15g,walltime=24:00:00
+#PBS -m abe
+#PBS -M haasx092@umn.edu
+#PBS -e 191119_snp_calling_subset.err
+#PBS -o 191119_snp_calling_subset.out
+#PBS -N 191119_snp_calling_subset
+
+cd /home/jkimball/haasx092/main_GBS
+
+module load samtools
+module load bcftools
+module load htslib
+module load parallel
+
+bams='present_natural_stands_only.txt'
+prefix="191119_samtools"
+parallel_samtools_processes=15
+ref="/home/jkimball/mshao/genome_seq/zizania_palustris_13Nov2018_okGsv.fasta"
+
+mkdir -p $prefix
+cut -f 1 ${ref}.fai \
+ | parallel --will-cite -I'{}' -j $parallel_samtools_processes \
+  \(samtools mpileup -q 40 -gDVu \
+  -b $bams \
+  -r '{}' \
+  -f ${ref} \
+  \| bcftools call -mv \
+  \| bgzip -c \
+  \> $prefix/${prefix}_'{}'.vcf.gz \) \
+  2\> $prefix/${prefix}_'{}'.err
+
