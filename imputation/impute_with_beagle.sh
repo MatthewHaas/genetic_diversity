6160bef39dd6374d6c9a84c7accdcdad83fec21e
Matthew Haas <matthew.haas1@gmail.com>
2020-01-29 15:51:03 -0600

    Initial commit
    Initial commit of script using BEAGLE to impute missing SNPs into GBS data.

---
+++ imputation/impute_with_beagle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#PBS -l nodes=1:ppn=8,mem=60g,walltime=24:00:00
+#PBS -m abe
+#PBS -M haasx092@umn.edu
+#PBS -e impute_with_beagle.err
+#PBS -o impute_with_beagle.out
+#PBS -N impute_with_beagle
+
+# 29 January 2020
+# This code is for using beagle to impute missing data from GBS data
+# WD: /home/jkimball/haasx092/main_GBS/imputation/200129_imputation
+
+cd /home/jkimball/haasx092/main_GBS/imputation/200129_imputation
+
+# Beagle files are located here: /home/jkimball/haasx092/beagle
+
+# Run beagle (setting an upper limit of 60Gb of memory)
+java -Xmx60g -jar ~/beagle/beagle.25Nov19.28d.jar gt=Scaffold_1.vcf.gz out=RESULT impute=TRUE
