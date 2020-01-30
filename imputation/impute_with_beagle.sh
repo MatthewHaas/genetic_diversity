1d8b5b21d19f41c91829909eea83cde110ec6da3
Matthew Haas <matthew.haas1@gmail.com>
2020-01-29 22:02:41 -0600

    Increased memory & changed input file
    Changes were made in response to error messages that suggested memory error. The first time I corrected the script, I only altered the max memory in the PBS script header, but not in the actual command (-Xmx100g). The recoded VCF file (filtered for bi-allelic sites only) is also much smaller than the original/raw VCF file that was being used (90M vs 251M).

--- imputation/impute_with_beagle.sh
+++ imputation/impute_with_beagle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -l nodes=1:ppn=8,mem=60g,walltime=24:00:00
+#PBS -l nodes=1:ppn=8,mem=100g,walltime=24:00:00
 #PBS -m abe
 #PBS -M haasx092@umn.edu
 #PBS -e impute_with_beagle.err
@@ -14,5 +14,5 @@ cd /home/jkimball/haasx092/main_GBS/imputation/200129_imputation
 
 # Beagle files are located here: /home/jkimball/haasx092/beagle
 
-# Run beagle (setting an upper limit of 60Gb of memory)
-java -Xmx60g -jar ~/beagle/beagle.25Nov19.28d.jar gt=Scaffold_1.vcf.gz out=RESULT impute=TRUE
+# Run beagle (setting an upper limit of 100Gb of memory)
+java -Xmx100g -jar ~/beagle/beagle.25Nov19.28d.jar gt=Scaffold_1.recode.vcf out=RESULT impute=TRUE
