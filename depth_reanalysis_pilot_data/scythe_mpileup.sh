fb4aa9ba92bcecbea8bc03545ff5aeb1b4e90998
Matthew Haas <matthew.haas1@gmail.com>
2020-05-11 14:36:28 -0500

    Initial commit

---
+++ depth_reanalysis_pilot_data/scythe_mpileup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+#PBS -l nodes=1:ppn=8,mem=22g,walltime=24:00:00
+#PBS -m abe
+#PBS -M haasx092@umn.edu
+#PBS -e scythe_mpileup.err
+#PBS -N scythe_mpileup
+
+cd /home/jkimball/haasx092/pilot_GBS
+
+module load samtools
+module load bcftools
+module load htslib
+module load parallel
+
+export bams="bam_list"
+export prefix="200511_samtools"
+export ref="/home/jkimball/mshao/genome_seq/zizania_palustris_13Nov2018_okGsv.fasta"
+
+parallel_samtools_processes=15
+
+mkdir -p $prefix
+cut -f 1 ${ref}.fai
+
+
+scythe_mpileup() {
+	REGIONS=${1}
+	SCAFFOLD=$(echo ${REGIONS} | cut -f 1 -d ";")
+	samtools mpileup -q 20 -gDVu \
+		-b $bams \
+		-r ${REGIONS} \
+		-f ${ref} \
+		| bcftools call -mv \
+		| bgzip -c \
+		> $prefix/${prefix}_${SCAFFOLD}.vcf.gz \
+		2> $prefix/${prefix}_${SCAFFOLD}.err
+}
+  
+export -f scythe_mpileup
+  
+parallel --will-cite -j $parallel_samtools_processes scythe_mpileup ::: $(cut -f 1 ${ref}.fai)
