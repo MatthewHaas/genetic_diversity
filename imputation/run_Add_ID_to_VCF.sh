fbe8174d5a9314eb97c2b09fa0ccbcbfa1b7a685
Matthew Haas <matthew.haas1@gmail.com>
2020-09-21 13:58:52 -0500

    Added missing "$" to variable name

--- imputation/run_Add_ID_to_VCF.sh
+++ imputation/run_Add_ID_to_VCF.sh
@@ -11,7 +11,7 @@ cd /home/jkimball/haasx092/main_GBS/200305_samtools
 module load python3
 
 # Call script. First argument is file name, second argument (zp) is the prefix (can change to be anything), third argument is the number of zeros to pad the SNP identifier
-for vcf_filename in $(cat vcf_list.txt); do
+for $vcf_filename in $(cat vcf_list.txt); do
 STEM=$(echo ${i} | cut -f 1 -d ".")
 python Add_ID_to_VCF.py vcf_filename zp 4 > ${STEM}_with_IDs.vcf
 done
