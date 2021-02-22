6e21a0053ff21fc133cdb2e3e2b5e1c3e7e1a924
Matthew Haas <matthew.haas1@gmail.com>
2021-02-21 22:06:17 -0600

    Initial commit

---
+++ imputation/210208_imputation/reformat_imputed_vcf_filt_by_pos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o reformat_vcf_sep_filt_snp_pos.out
+#SBATCH -e reformat_vcf_sep_filt_snp_pos.err
+
+cd /home/jkimball/haasx092/main_GBS/imputation/210208_imputation
+
+for i in $(cat imputed_vcf_filt_by_pos.txt); do
+STEM=$(echo ${i} | cut -f 1,2,3,4,14,15,16,17,18,19,20 -d "_" | cut -f 1 -d ".") #shorten lengthy file name by underscore and get rid of filename extension
+zcat $i | sed 's/|/\//g' > ${STEM}_sep_fixed.vcf
+done
