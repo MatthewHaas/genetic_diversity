0ce5b2aa3312a06f3babbb60b8cc6b00a3ea4f40
Matthew Haas <matthew.haas1@gmail.com>
2021-02-21 22:14:19 -0600

    Change name of awk script

--- imputation/210208_imputation/run_normalize_awk_for_filtered_imputed.sh
+++ imputation/210208_imputation/run_normalize_awk_for_filtered_imputed.sh
@@ -15,5 +15,5 @@ cd /home/jkimball/haasx092/main_GBS/imputation/210208_imputation
 prefix="200305_samtools"
 normalize_prefix="210221_normalize"
 mktemp | read tmp
-cat ${prefix}_*imputed_with_ref_filt_by_snp_pos_sep_fixed.vcf | awk -f ./normalize.awk > '$tmp' 2> $normalize_prefix.err
+cat ${prefix}_*imputed_with_ref_filt_by_snp_pos_sep_fixed.vcf | awk -f ./normalize_modified_for_imputed_snps.awk > '$tmp' 2> $normalize_prefix.err
 cp '$tmp' 210221_normalize_filt_snp_pos.tsv
