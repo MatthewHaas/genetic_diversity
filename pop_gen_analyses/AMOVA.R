de79b526628f9f4ef5bbeff3111f334c71c644e6
Matthew Haas <matthew.haas1@gmail.com>
2021-03-11 14:53:51 -0600

    Update AMOVA.R

--- pop_gen_analyses/AMOVA.R
+++ pop_gen_analyses/AMOVA.R
@@ -1,16 +1,32 @@
-# 6 February 2020
-# WD: /home/jkimball/haasx092/main_GBS/imputation/200129_imputation
+# 11 March 2021
+# WD: /home/jkimball/haasx092/main_GBS/210306_samtools
 # Purpose of this code is to perform an AMOVA (analysis of molecular variance)
 
 # Load required packages
+library(vcfR)
 library(poppr)
+library(ape)
+library(RColorBrewer)
 library(data.table)
 
-# Load R package that contains the genlight object needed to do AMOVA
-load("200204_popgen_with_poppr_imputed.Rdata")
+x <- read.vcfR("filt_20_NA_vcf_files_concat.vcf")
 
 # Load sample key
- y <- fread("~/main_GBS/191021_main_GBS_sample_key.csv")
+y <- fread("~/main_GBS/191021_main_GBS_sample_key.csv")
+
+# Convert to a genlight object (for poppr and adegenet)
+gen_light_x <- vcfR2genlight(x)
+# Remove Itasca "haploid" samples
+gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_1039/Sample_1039_sorted.bam"]
+gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_1045/Sample_1045_sorted.bam"]
+
+# Figure out which of the samples are "NA" for ploidy which is currently causing an error at the AMOVA stage
+ploidy <- as.data.table(cbind(gen_light_x@ind.names, is.na(ploidy(gen_light_x))))
+ploidy[V2 == "TRUE"] # Samples 800, 875, and 962
+# Remove those samples
+gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_0800/Sample_0800_sorted.bam"]
+gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_0875/Sample_0875_sorted.bam"]
+gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_0962/Sample_0962_sorted.bam"]
 
 # Remove unnecessary columns and rename columns for defined strata
 y[, ID := NULL]
@@ -26,7 +42,12 @@ pilot_GBS <- c("Sample_1047", "Sample_1048", "Sample_1049", "Sample_1050", "Samp
 y[!(sample %in% pilot_GBS)] -> y # This should be read as "sample not in pilot_GBS"
 
 # One of the Itasca "haploids" was also removed from the subset being analyzed here..
-y[sample != "Sample_1045"] -> y
+y[sample != "Sample_0837"] -> y # this is the "uknown" line
+y[sample != "Sample_1045"] -> y # Itasca "haploid"
+y[sample != "Sample_1039"] -> y # Itasca "haploid
+y[sample != "Sample_0800"] -> y
+y[sample != "Sample_0875"] -> y
+y[sample != "Sample_0962"] -> y
 
 # Not sure if this function actually did anything??
 strata(gen_light_x, formula = ~sample/population/class, value = y)
