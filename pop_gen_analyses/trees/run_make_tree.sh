6de34b9e4e5eb1472ad0e024e360bd7929051073
Matthew Haas <matthew.haas1@gmail.com>
2021-03-14 23:23:11 -0500

    Add files via upload

---
+++ pop_gen_analyses/trees/run_make_tree.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o run_make_tree.out
+#SBATCH -e run_make_tree.err
+
+cd /home/jkimball/haasx092/main_GBS/210309_samtools
+
+module load gcc/8.1.0
+module load udunits/2.2.26_gcc8.1.0
+module load proj/4.9.3
+module load gdal/2.3.2
+module load geos/3.7.1
+
+module load R/3.6.0
+
+# arg[1] is a VCF file; arg[2] os a CSV file containing population data; arg[3] is Rdata file you want to save to; arg[4] is the PDF file to save the plot
+Rscript make_tree.R natural_stands.recode.vcf natural_stand_samples.csv natural_stand_tree.Rdata natural_stand_tree.pdf
---
+++ pop_gen_analyses/trees/run_make_tree_breeding_lines.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o run_make_tree_breeding_lines.out
+#SBATCH -e run_make_tree_breeding_lines.err
+
+cd /home/jkimball/haasx092/main_GBS/210306_samtools
+
+module load gcc/8.1.0
+module load udunits/2.2.26_gcc8.1.0
+module load proj/4.9.3
+module load gdal/2.3.2
+module load geos/3.7.1
+
+module load R/3.6.0
+
+# arg[1] is a VCF file; arg[2] os a CSV file containing population data; arg[3] is Rdata file you want to save to; arg[4] is the PDF file to save the plot
+Rscript make_tree_breeding_lines.R breeding_lines.recode.vcf breeding_lines.csv breeding_lines_tree.Rdata breeding_lines_tree.pdf
