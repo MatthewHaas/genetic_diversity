306f58ed63ae0a9e2116d011c9a22b1cfe62809a
Matthew Haas <matthew.haas1@gmail.com>
2021-03-14 22:58:25 -0500

    Commit shell scripts

---
+++ pop_gen_analyses/trees/filter_with_vcftools_by_ind_temporal_samples.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o filter_vcfs_temporal_samples.out
+#SBATCH -e filter_vcfs_temporal_samples.err
+
+# Include path to the working directory
+cd /home/jkimball/haasx092/main_GBS/210306_samtools
+
+# Filter VCF files to keep natural stands only
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_short_sample_names.vcf --keep temporal_samples_for_vcftools_filt.txt --recode --recode-INFO-all --out temporal_samples
