6059204b41ee401e8f49a0ad7ddeaa78947833f0
Matthew Haas <matthew.haas1@gmail.com>
2021-03-24 16:46:50 -0500

    Initial commit

---
+++ pop_gen_analyses/minimum_spanning_trees/breeding_lines_mst.R
@@ -0,0 +1,20 @@
+library(adegenet)
+library(vcfR)
+library(poppr)
+library(ape)
+library(RColorBrewer)
+library(data.table)
+library(igraph)
+
+load("breeding_lines_tree_dp4.Rdata")
+rubi.dist <- bitwise.dist(gen_light_x)
+rubi.msn <- poppr.msn(gen_light_x, rubi.dist, showplot = FALSE, include.ties = T)
+
+node.size <- rep(2, times = nInd(gen_light_x))
+names(node.size) <- indNames(gen_light_x)
+vertex.attributes(rubi.msn$graph)$size <- node.size
+
+set.seed(55)
+pdf("breeding_lines_minimum_spanning_network.pdf", height = 30, width = 30)
+plot_poppr_msn(gen_light_x, rubi.msn , palette =c("green3", "burlywood", "purple4", "hotpink"), gadj = 20)
+dev.off()
