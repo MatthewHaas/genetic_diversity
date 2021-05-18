6079202a8086b0f1564d60f7c1debcefa4dc29cf
Matthew Haas <matthew.haas1@gmail.com>
2021-05-17 21:26:11 -0500

    Update AMOVA_with_genclone.R

--- pop_gen_analyses/AMOVA_with_genclone.R
+++ pop_gen_analyses/AMOVA_with_genclone.R
@@ -4,25 +4,33 @@ library(poppr)
 library(ape)
 library(RColorBrewer)
 library(data.table)
+
+# Load previously saved R data
 load("natural_stands_genind_obj.Rdata")
+
+# Read in sample data
 y <- fread("~/main_GBS/191021_main_GBS_sample_key.csv")
+
 # Remove unnecessary columns and rename columns for defined strata
 y[, ID := NULL]
 y[, sample_ID_extended := NULL]
 setnames(y, c("sample", "population", "class"))
-# Remove samples that are part of the overall GBS project, but are not in the subset being analyzed.
-y[population != "Johnson X Dora F4"] -> y
-y[population != "Dovetail"] -> y
-# Remove pilot GBS samples
-pilot_GBS <- c("Sample_1047", "Sample_1048", "Sample_1049", "Sample_1050", "Sample_1051", "Sample_1052", "Sample_1053", "Sample_1054")
-y[!(sample %in% pilot_GBS)] -> y # This should be read as "sample not in pilot_GBS"
-# One of the Itasca "haploids" was also removed from the subset being analyzed here..
-y[sample != "Sample_0837"] -> y # this is the "uknown" line
-y[sample != "Sample_1045"] -> y # Itasca "haploid"
-y[sample != "Sample_1039"] -> y # Itasca "haploid
-y[sample != "Sample_0800"] -> y
-y[sample != "Sample_0875"] -> y
-y[sample != "Sample_0962"] -> y
-y <- y[class == "Natural stand" & population != "Big Fork River" & population != "Garfield Lake Old" & population != "Shell Lake Old"]
-genclone_x <- as.genclone(gen_ind_x)
-results <- poppr.amova(genclone_x, ~population, clonecorrect = TRUE)
+
+# subset genind object to only retain Z. aquatica and Bass Lake samples
+aquatica_and_bass_lake_genind <- gen_ind_x[i=c(193:195, 204:206, 215:217, 226:228, 238:240, 250:252, 261:264, 273:286, 289:298, 301:310, 313:322, 325:333, 336:344, 347:354, 358:365)]
+
+# Subset population key and change AquaticaSpecies/Aquatica_species to something more appropriate (Z. aquatica)
+y <- y[population == "Bass Lake" | population == "AquaticaSpecies" | population == "Aquatica_species"]
+y[population == "AquaticaSpecies", population := "Z aquatica"]
+y[population == "Aquatica_species", population := "Z aquatica"]
+
+# Add population info (strata) to genind object
+strata(aquatica_and_bass_lake_genind) <- data.frame(y)
+
+# Convert genind to genclone
+aquatica_and_bass_lake_genclone <- as.genclone(aquatica_and_bass_lake_genind)
+
+# Do the AMOVA
+results <- poppr.amova(aquatica_and_bass_lake_genclone, ~ population, within = FALSE, clonecorrect = TRUE)
+
+# next, get p-value
