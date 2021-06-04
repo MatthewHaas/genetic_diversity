006298e8e5f001780cf5174802c9f1619f696ec3
Matthew Haas <matthew.haas1@gmail.com>
2021-06-03 22:45:23 -0500

    Update hudson_fst.R

--- pop_gen_analyses/hudson_fst.R
+++ pop_gen_analyses/hudson_fst.R
@@ -2,11 +2,17 @@
 library(data.table)
 library(KRIS)
 
+source("fst_hudson_function.R")
+
+args = commandArgs(trailingOnly = TRUE)
+
 # Read in data
-data <- fread(filename)
+data <- fread(args[1])
 
 # Convert to matrix
 data_mat <- as.matrix(data)
 
 # Calculate Fst using Hudson method
-fst.hudson(data_mat, c(1:50), c(51:100))
+x <- fst.hudson(data_mat, c(1:50), c(51:100))
+
+write.table(paste0(x, "\t", toString(args[1])), file=args[2], row.names = FALSE, col.names = FALSE, append = TRUE)
