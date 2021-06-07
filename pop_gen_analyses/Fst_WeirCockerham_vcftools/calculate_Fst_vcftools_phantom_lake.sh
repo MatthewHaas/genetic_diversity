2479879572c1bab797b710f5eb0e3ff7189fdacf
Matthew Haas <matthew.haas1@gmail.com>
2021-06-07 15:43:34 -0500

    Initial commit

---
+++ pop_gen_analyses/Fst_WeirCockerham_vcftools/calculate_Fst_vcftools_phantom_lake.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o calculate_Fst_vcftools_phantom_lake_comparisons.out
+#SBATCH -e calculate_Fst_vcftools_phantom_lake_comparisons.err
+
+cd /home/jkimball/haasx092/Fst_vcftools
+
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat.vcf --weir-fst-pop phantom_lake.txt --weir-fst-pop plantagenet.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat.vcf --weir-fst-pop phantom_lake.txt --weir-fst-pop shell_lake.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat.vcf --weir-fst-pop phantom_lake.txt --weir-fst-pop upper_rice_lake.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat.vcf --weir-fst-pop phantom_lake.txt --weir-fst-pop zaquatica.txt
---
+++ pop_gen_analyses/Fst_WeirCockerham_vcftools/calculate_Fst_vcftools_shell_lake.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o calculate_Fst_vcftools_shell_lake_comparisons.out
+#SBATCH -e calculate_Fst_vcftools_shell_lake_comparisons.err
+
+cd /home/jkimball/haasx092/Fst_vcftools
+
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat.vcf --weir-fst-pop shell_lake.txt --weir-fst-pop upper_rice_lake.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat.vcf --weir-fst-pop shell_lake.txt --weir-fst-pop zaquatica.txt
