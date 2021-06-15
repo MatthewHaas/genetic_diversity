3d75cdef34ebe4522ff562209b2825249fd11079
Matthew Haas <matthew.haas1@gmail.com>
2021-06-14 22:59:08 -0500

    Initial commit

---
+++ pop_gen_analyses/convert_csv_to_arlequin_input_format.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+infile = open(sys.argv[1], 'r')
+outfile = open(sys.argv[2], 'w')
+
+allele1 = list()
+allele2 = list()
+
+data = infile.readlines()
+
+for line in data:
+        line = line.split(',')
+        for char in line:
+                if char == 0:
+                        allele1.append(line[3])
+                        allele2.append(line[3])
+                elif char == 1:
+                        allele1.append(line[3])
+                        allele2.append(line[4])
+                elif char == 2:
+                        allele1.append(line[4])
+                        allele2.append(line[4])
+                elif char == 'NA':
+                        allele1.append(str('?'))
+                        allele2.append(str('?'))
+                outfile.write(' '.join(allele1) + '\n')
+                outfile.write(' '.join(allele2) + '\n')
+outfile.close()
