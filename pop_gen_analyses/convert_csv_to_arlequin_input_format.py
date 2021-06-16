fe18d6b571f1df7b2863fea1a76d26fdad72737f
Matthew Haas <matthew.haas1@gmail.com>
2021-06-15 21:22:20 -0500

    Updated script

--- pop_gen_analyses/convert_csv_to_arlequin_input_format.py
+++ pop_gen_analyses/convert_csv_to_arlequin_input_format.py
@@ -9,21 +9,25 @@ allele2 = list()
 
 data = infile.readlines()
 
+firstline = True
 for line in data:
-        line = line.split(',')
-        for char in line:
-                if char == 0:
-                        allele1.append(line[3])
-                        allele2.append(line[3])
-                elif char == 1:
-                        allele1.append(line[3])
-                        allele2.append(line[4])
-                elif char == 2:
-                        allele1.append(line[4])
-                        allele2.append(line[4])
-                elif char == 'NA':
-                        allele1.append(str('?'))
-                        allele2.append(str('?'))
-                outfile.write(' '.join(allele1) + '\n')
-                outfile.write(' '.join(allele2) + '\n')
+    if firstline:
+        firstline = False
+        continue
+    line = line.split(',')
+    for char in line[5:]:
+        if char == '0':
+            allele1.append(line[3])
+            allele2.append(line[3])
+        elif char == '1':
+            allele1.append(line[3])
+            allele2.append(line[4])
+        elif char == '2':
+            allele1.append(line[4])
+            allele2.append(line[4])
+        elif char == 'NA':
+            allele1.append(str('?'))
+            allele2.append(str('?'))
+    outfile.write(' '.join(allele1) + '\n')
+    outfile.write(' '.join(allele2) + '\n')
 outfile.close()
