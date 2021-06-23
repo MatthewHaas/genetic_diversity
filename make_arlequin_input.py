f01d5f0642e4d1f71d3b60a19f663957ad01df23
Matthew Haas <matthew.haas1@gmail.com>
2021-06-23 15:46:12 -0500

    Update

---
+++ make_arlequin_input.py
@@ -0,0 +1,67 @@
+import sys
+import os
+
+infile = open(sys.argv[1], 'r')
+outfile = open(sys.argv[2], 'w')
+
+allele1 = list()
+allele2 = list()
+
+data = infile.readlines()
+
+firstline = True
+secondline = True
+thirdline = True
+fourthline = True
+fifthline = True
+for line in data:
+    if firstline:
+        firstline = False
+        continue
+    if secondline:
+        secondline = False
+        continue
+    if thirdline:
+        thirdline = False
+        continue
+    if fourthline:
+        fourthline = False
+        line = line.split(',')
+        refAllele = list()
+        for char in line[1:]:
+            refAllele.append(char)
+        continue
+    if fifthline:
+        fifthline = False
+        line = line.split(',')
+        altAllele = list()
+        for char in line[1:]:
+            altAllele.append(char)
+        continue
+    line = line.strip()
+    line = line.split(',')
+    sampleID = list()
+    for char in line:
+        index = 0
+        sampleID.append(line[0])
+        if char == '0':
+	    allele1.append(refAllele[index])
+	    allele2.append(refAllele[index])
+            index = index + 1
+        elif char == '1':
+	    allele1.append(refAllele[index])
+	    allele2.append(altAllele[index])
+            index = index + 1
+        elif char == '2':
+	    allele1.append(altAllele[index])
+	    allele2.append(altAllele[index])
+            index = index + 1
+        elif char == 'NA':
+	    allele1.append(str('?'))
+	    allele2.append(str('?'))
+            index = index + 1
+    outfile.write(sampleID[index] + ' 1 ' + ' '.join(allele1) + '\n')
+    outfile.write(' ' + ' ' + ' '.join(allele2) + '\n')
+    del allele1[:] # too bad can't use clear() method
+    del allele2[:] # too bad can't use clear() method
+outfile.close()
