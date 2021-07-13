c06b0ebc2181e1d1585ec24e1b053f7f35e43a0c
Matthew Haas <matthew.haas1@gmail.com>
2021-07-13 16:14:11 -0500

    Initial commit

---
+++ vcftools_scripts_incl_nonbiallelic/calculate_Fst_vcftools_necktie_river_incl_nonbiallelic_snps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o calculate_Fst_vcftools_necktie_river_comparisons_incl_nonbiallelic.out
+#SBATCH -e calculate_Fst_vcftools_necktie_river_comparisons_incl_nonbiallelic.err
+
+cd /home/jkimball/haasx092/Fst_vcftools
+
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_snps.vcf --weir-fst-pop necktie_river.txt --weir-fst-pop ottertail_river.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_snps.vcf --weir-fst-pop necktie_river.txt --weir-fst-pop phantom_lake.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_snps.vcf --weir-fst-pop necktie_river.txt --weir-fst-pop plantagenet.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_snps.vcf --weir-fst-pop necktie_river.txt --weir-fst-pop shell_lake.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_snps.vcf --weir-fst-pop necktie_river.txt --weir-fst-pop upper_rice_lake.txt
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_snps.vcf --weir-fst-pop necktie_river.txt --weir-fst-pop zaquatica.txt
