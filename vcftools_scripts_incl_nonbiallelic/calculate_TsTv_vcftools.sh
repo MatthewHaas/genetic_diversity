87976b22f8f5824bb6bcb290cdd85d012e75e6fc
Matthew Haas <matthew.haas1@gmail.com>
2021-08-27 16:39:51 -0500

    Initial commit

---
+++ vcftools_scripts_incl_nonbiallelic/calculate_TsTv_vcftools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o calculate_TsTv_vcftools.out
+#SBATCH -e calculate_TsTv_vcftools.err
+
+cd /home/jkimball/haasx092/TajimaD
+
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_snps.vcf --TsTv 1000000
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_snps.vcf --TsTv-summary
