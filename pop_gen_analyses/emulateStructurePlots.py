a8f44bd68482179d313f9e1dea29040fef30f254
Matthew Haas <matthew.haas1@gmail.com>
2021-09-12 21:28:48 -0500

    Add code block for K=3

--- pop_gen_analyses/emulateStructurePlots.py
+++ pop_gen_analyses/emulateStructurePlots.py
@@ -29,6 +29,31 @@ def makePlot(K = NULL):
         plt.text(x2, y, s = cluster2_ID[0], fontsize = 12, ha = 'center')
         plt.savefig(sys.argv[2])
     elif K == 3:
+        existing_IDs = [] # empty list to store existing IDs. Main purpose is to have a mechanism to recogize of 'Natural stand' has already been used and 'Natural stand II' should be used instead
+        cluster1_ID = dfSorted[(dfSorted['Most_likely'] == 'Cluster_1') & (dfSorted['Likelihood'] > 0.6)].Class.mode()
+        existing_IDs.append(cluster1_ID[0])
+        cluster2_ID = dfSorted[(dfSorted['Most_likely'] == 'Cluster_2') & (dfSorted['Likelihood'] > 0.6)].Class.mode()
+        existing_IDs.append(cluster2_ID[0])
+        cluster3_ID = dfSorted[(dfSorted['Most_likely'] == 'Cluster_3') & (dfSorted['Likelihood'] > 0.6)].Class.mode()
+        if cluster3_ID[0] in existing_IDs:
+            cluster3_ID[0] = 'Natural stand II'
+            existing_IDs.append(cluster3_ID[0])
+        else:
+            existing_IDs.append(cluster3_ID[0])
+        plt.figure(figsize = (20, 4.8)) # supposedly this is in inches...
+        plt.xticks([])
+        plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_2'], color = colorsAll[0], width = 1)
+        plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_1'], bottom = dfSorted['Cluster_2'], color = colorsAll[1], width = 1)
+        plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_3'], bottom = dfSorted['Cluster_2'] + dfSorted['Cluster_1'], color = colorsAll[2], width = 1)
+        plt.ylabel('Population membership probability')
+        x1 = len(dfSorted[(dfSorted['Most_likely'] == 'Cluster_1') & (dfSorted['Likelihood'] > 0.6)])/2
+        x2 = x1*2 + len(dfSorted[(dfSorted['Most_likely'] == 'Cluster_2') & (dfSorted['Likelihood'] > 0.6)])/2
+        x3 = x1*2 + + len(dfSorted[(dfSorted['Most_likely'] == 'Cluster_2') & (dfSorted['Likelihood'] > 0.6)]) + len(dfSorted[(dfSorted['Most_likely'] == 'Cluster_3') & (dfSorted['Likelihood'] > 0.6)])/2
+        y = -0.1 # constant--different y values are not necessary for plotting text
+        plt.text(x1, y, s = cluster1_ID[0], fontsize = 12, ha = 'center')
+        plt.text(x2, y, s = cluster2_ID[0], fontsize = 12, ha = 'center')
+        plt.text(x3, y, s = cluster3_ID[0], fontsize = 12, ha = 'center')
+        plt.savefig(sys.argv[2])
     elif K == 4:
     elif K == 5:
     elif K == 6:
