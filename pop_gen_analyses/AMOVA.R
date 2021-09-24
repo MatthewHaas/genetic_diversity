ceffad6d36bd862c559ee633a2416b63ffb4625b
Matthew Haas <matthew.haas1@gmail.com>
2021-09-23 22:06:11 -0500

    Update with consensus parameters and comparisons

--- pop_gen_analyses/AMOVA.R
+++ pop_gen_analyses/AMOVA.R
@@ -1,5 +1,5 @@
-# 11 March 2021
-# WD: /home/jkimball/haasx092/main_GBS/210306_samtools
+# 23 September 2021
+# WD: /home/jkimball/haasx092/AMOVA
 # Purpose of this code is to perform an AMOVA (analysis of molecular variance)
 
 # Load required packages
@@ -9,9 +9,12 @@ library(ape)
 library(RColorBrewer)
 library(data.table)
 
-x <- read.vcfR("filt_20_NA_vcf_files_concat.vcf")
+args = commandArgs(trailingOnly = TRUE)
 
-# Load sample key
+x <- read.vcfR(args[1])
+
+# Load sample key--updated key has STRUCTURE populations defined
+#y <- fread("~/main_GBS/191021_main_GBS_sample_key.csv")
 y <- fread("210920_sample_key_with_population_structure.csv")
 
 # Convert to a genlight object (for poppr and adegenet)
@@ -23,6 +26,7 @@ gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_1045/Sample_1045_sor
 # Figure out which of the samples are "NA" for ploidy which is currently causing an error at the AMOVA stage
 ploidy <- as.data.table(cbind(gen_light_x@ind.names, is.na(ploidy(gen_light_x))))
 ploidy[V2 == "TRUE"] # Samples 800, 875, and 962
+
 # Remove those samples
 gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_0800/Sample_0800_sorted.bam"]
 gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_0875/Sample_0875_sorted.bam"]
@@ -31,7 +35,7 @@ gen_light_x <- gen_light_x[indNames(gen_light_x) != "Sample_0962/Sample_0962_sor
 # Remove unnecessary columns and rename columns for defined strata
 y[, ID := NULL]
 y[, sample_ID_extended := NULL]
-setnames(y, c("sample", "population", "class"))
+setnames(y, c("sample", "population", "class", "STRUCTURE_K3", "STRUCTURE_K4", "STRUCTURE_K5"))
 
 # Remove samples that are part of the overall GBS project, but are not in the subset being analyzed.
 y[population != "Johnson X Dora F4"] -> y
@@ -48,14 +52,70 @@ y[sample != "Sample_1039"] -> y # Itasca "haploid
 y[sample != "Sample_0800"] -> y
 y[sample != "Sample_0875"] -> y
 y[sample != "Sample_0962"] -> y
+y[sample != "Sample_0975"] -> y # Remove this one due to missing data
 
+y[population != "Big Fork River"] -> y # Remove "Big Fork River" samples
+y[population != "Garfield Lake Old"] -> y # Remove old (2010) Garfield Lake (temporal) samples
+y[population != "Shell Lake Old"] -> y # Remove old (2010) Shell Lake (temporal) samples
 # Not sure if this function actually did anything??
-strata(gen_light_x, formula = ~sample/population/class, value = y)
+strata(gen_light_x, formula = ~population, value = y)
 
 # Add strata to the GENLIGHT object gen_light_x
 strata(gen_light_x) <- y
 
-# Do the AMOVA
-# A few notes: as of today (7 February 2020), all three algorithms and both methods are producing identical results.
-# I would expect some variation between these different options, so more specific parameters are probably needed in this code to achieve different results.
-poppr.amova(gen_light_x, hier = ~population/class, algorithm = "average_neighbor", method = "pegas")
+# Get the names to agree with one another
+# Turns out, this didn't do anything...but I think the issue might be that I need to ID the samples in a different manner
+for (i in y$sample){
+y[sample == i, sample := paste0(i, "/", i, "_sorted.bam")]
+}
+
+# Make a genlight object for natural stands only
+natural_stand_gl <-  gen_light_x[gen_light_x@strata$class != "Breeding line"]
+natural_stand_table <- y[class == "Natural stand"]
+strata(natural_stand_gl, formula = ~population, value = natural_stand_table)
+
+# Do the same for cultivated material
+cultivated_gl <- gen_light_x[gen_light_x@strata$class != "Natural stand"]
+cultivated_table <- y[class == "Breeding line"]
+strata(cultivated_gl, formula = ~population, value = cultivated_table)
+
+# Run AMOVAs
+# quasieuclid method is the default correction for non-euclidean distance, but is explicitly included to remove all doubt from the mind of anyone who may read this code
+natural_stand_results <- poppr.amova(natural_stand_gl, hier = ~population, algorithm = "farthest_neighbor", within = FALSE, method = "ade4", correction = "quasieuclid")
+cultivated_results <- poppr.amova(cultivated_gl, hier = ~population, algorithm = "farthest_neighbor", within = FALSE, method = "ade4", correction = "quasieuclid")
+natural_stand_v_cultivated_results <-poppr.amova(gen_light_x, hier = ~class, algorithm = "farthest_neighbor", within = FALSE, method = "ade4", correction = "quasieuclid")
+STRUCTURE_K5_results <- poppr.amova(gen_light_x, hier = ~STRUCTURE_K5, algorithm = "farthest_neighbor", within = FALSE, method = "ade4", correction = "quasieuclid")
+
+# print out results of AMOVA
+print("natural_stand results:")
+natural_stand_results
+print("cultivated results:")
+cultivated_results
+print("natural_stand vs. cultivated results:")
+natural_stand_v_cultivated_results
+print("STRUCTURE_K5_results:")
+STRUCTURE_K5_results
+
+# significance testing
+set.seed(999)
+natural_stand_results_signif <- randtest(natural_stand_results, nrepet = 999)
+cultivated_results_signif <- randtest(cultivated_results, nrepet = 999)
+natural_stand_v_cultivated_results_signif <- randtest(natural_stand_v_cultivated_results, nrepet = 999)
+STRUCTURE_K5_results_signif <- randtest(STRUCTURE_K5_results, nrepet = 999)
+
+
+# plot results of significance testing
+plot(natural_stand_results_signif)
+plot(cultivated_results_signif)
+plot(natural_stand_v_cultivated_results_signif)
+plot(STRUCTURE_K5_results_signif)
+
+# print out results of significance testing
+print("natural stand results significance:")
+natural_stand_results_signif
+print("cultivated results significance:")
+cultivated_results_signif
+print("natural stands vs cultivated  significance:")
+natural_stand_v_cultivated_results_signif
+print("STRUCTURE_K5_results significance:")
+STRUCTURE_K5_results_signif
