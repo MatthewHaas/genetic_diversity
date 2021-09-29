dc71a65467034a326a73a219d05d8a41135cf472
Matthew Haas <matthew.haas1@gmail.com>
2021-09-28 19:12:43 -0500

    Initial commit

---
+++ pop_gen_analyses/trees/filter_with_vcftools_by_ind_natural_stands_and_breed_lines.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=30g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=haasx092@umn.edu
+#SBATCH -p amdsmall
+#SBATCH --account=jkimball
+#SBATCH -o filter_vcfs_natural_stnads_and_breeding_lines.out
+#SBATCH -e filter_vcfs_natural_stands_and_breeding_lines.err
+
+# Include path to the working directory
+cd /home/jkimball/haasx092/trees_for_diversity_study
+
+# Filter VCF files to keep natural stands only
+~/vcftools/bin/vcftools --gzvcf filt_20_NA_vcf_files_concat_incl_nonbiallelic_short_sample_names.vcf --keep natural_stands_and_breeding_lines_for_vcftools_filt.txt --recode --recode-INFO-all --out natural_stands_and_breeding_lines
