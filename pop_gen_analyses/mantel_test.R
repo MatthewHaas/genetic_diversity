f52aed270ab3c0bffb303127110067968b924e0e
Matthew Haas <matthew.haas1@gmail.com>
2021-10-25 20:15:15 -0500

    Update to include Itasca-C12

--- pop_gen_analyses/mantel_test.R
+++ pop_gen_analyses/mantel_test.R
@@ -2,8 +2,8 @@ library(data.table)
 library(ade4)
 
 # Load data
-geo <- fread("geo_distances.csv")
-genet <- fread("genetic_distances.csv")
+geo <- fread("geo_distances_incl_ItascaC12.csv")
+genet <- fread("genetic_distances_incl_ItascaC12.csv")
 
 # Convert to distance object
 geo <- dist(geo)
@@ -13,10 +13,10 @@ genet <- dist(genet)
 mantel <- mantel.rtest(geo, genet, nrepet = 9999)
 
 # Make plots
-pdf("211019_mantel_test.pdf")
+pdf("211025_mantel_test_incl_ItascaC12.pdf")
 plot(mantel, xlab = "Simulated correlation", main = "Mantel test", las = 1)
 dev.off()
 
-pdf("211019_dotplot_for_mantel.pdf")
+pdf("211025_dotplot_for_mantel_incl_ItascaC12.pdf")
 plot(geo, genet, xlab = "Geographic distance", ylab = "Genetic distance", main = "Correlation between genetic and geographical distance", las = 1, pch = 16)
 dev.off()
