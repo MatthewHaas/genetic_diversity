526d9fbcfee32b0c98a0197c1f34cd5495298cfa
Matthew Haas <matthew.haas1@gmail.com>
2021-10-27 21:52:05 -0500

    add commentary

--- pop_gen_analyses/jaccard_similarity.R
+++ pop_gen_analyses/jaccard_similarity.R
@@ -10,6 +10,8 @@ Sample_0002 <- as.vector(as.matrix(data[2])[1,-1])
 Sample_0003 <- as.vector(as.matrix(data[3])[1,-1])
 
 # Define Jaccard Similarity function
+# This doesn't really work for GBS data--the intersection only yield a length of 4 (genotype values 0, 1, 2, and the missing value "NA")
+# As a result, the jaccard value is absurdly low
 jaccard <- function(a, b) {
     intersection = length(intersect(a, b))
     union = length(a) + length(b) - intersection
