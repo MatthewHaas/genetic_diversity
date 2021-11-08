9f44f37f0413fb223c5d9459e07a8b79e0975bfc
Matthew Haas <matthew.haas1@gmail.com>
2021-11-08 16:07:36 -0600

    update--returning values that are closer to expectation
    This code is working and returns reasonable similarity values (Sample_0001 vs Sample_0001 is 1, etc). But I think this is still not returning true jaccard values. Instead, it seems to be more counting simple matches/mismatches.

--- pop_gen_analyses/similarity.py
+++ pop_gen_analyses/similarity.py
@@ -4,10 +4,12 @@ import sys
 import os
 
 # read in data
-data = read_csv(sys.argv[1], 'r', header = None) # csv file with one column of sample names and multiple columns of genotype calls (one column per variant site)
+data = read_csv(sys.argv[1], 'r', header = None, delimiter = ',') # csv file with one column of sample names and multiple columns of genotype calls (one column per variant site)
+# for example, 211101_snp_data_formatted_for_similarity.csv
 
 # open output file
 outfile = open(sys.argv[2], 'w') # csv file with two columns of sample names (to indicate the pairwise comparison being made) and one column with a similarity score
+# for example, 21103_similarity_long_python_attempt2.csv
 
 # Make a list of sample names
 sampleNames = data[0].tolist()
@@ -17,7 +19,6 @@ sampleNames2 = sampleNames
 # Make a csv file with two columns for sample names to prep for pairwise similarity comparisons
 outfile.write('sampleName1' + ',' + 'sampleName2' + ',' + 'similarity' + '\n')
 
-
 for sample in sampleNames:
     for sample2 in sampleNames2:
         a = data[data[0] == sample]
@@ -25,9 +26,34 @@ for sample in sampleNames:
         a = a.iloc[0,1:]
         b = b.iloc[0,1:]
         count = 0
+        total = 0
         for i in range(1,5955):
-            if a[i] == b[i]:
-        	    count += 1
-        similarity = round(count/5955, 4)
+            if pd.isna(a[i]) or pd.isna(b[i]):
+                #print('NaN spotted at position '+ str(i))
+                continue
+            elif a[i] == b[i]:
+                if a[i] == 0 and b[i] == 0:
+                    count += 1
+                    total += 1
+                elif a[i] == 2 and b[i] == 2:
+                    count += 1
+                    total += 1
+                elif a[i] == 1 and b[i] == 1:
+                    count += 1
+                    total += 1
+                elif pd.isna(a[i]) or pd.na(b[i]):
+                    print('NaN spotted when a=b')
+                    continue
+                else:
+                    print('something else')
+                    continue
+            elif a[i] != b[i]:
+                #print(sample + ' is not equal to ' + sample2 + ' at position ' + str(i))
+                total += 1
+            else:
+                print('no conditions met') # this was included to make sure the loop wasn't going here because I didn't expect it to
+                continue
+        similarity = round(count/total, 4)
         outfile.write(str(sample) + ',' + str(sample2) + ',' + str(similarity) + '\n')
+                    
 outfile.close()
