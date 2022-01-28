87e7be19983154db9854146f5dd01c837040c56b
Matthew Haas <matthew.haas1@gmail.com>
2022-01-28 17:10:38 -0600

    Update admixtools_ABBA-BABA.R

--- pop_gen_analyses/ABBA-BABA/admixtools_ABBA-BABA.R
+++ pop_gen_analyses/ABBA-BABA/admixtools_ABBA-BABA.R
@@ -1,4 +1,14 @@
 library(admixtools) # for this analysis, you should be using R version 4.1.0
 library(tidyverse)
 
-# Read data in (plink format should work)
+# load data
+geno = read_plink("plink_binary")
+
+# assign cluster membership
+cluster1 <- scan("cluster_1.txt", character())
+aquatica <- scan("aquatica.txt", character())
+cultivated <- scan("cultivated_material.txt", character())
+cluster4 <- scan("cluster_4.txt", character())
+
+# run d-statistics
+f4(geno, cluster1, aquatica, cultivated, cluster4, f4mode = FALSE, sure = TRUE)
