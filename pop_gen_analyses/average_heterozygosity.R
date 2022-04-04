a1d0362737e96c2a1b8b31cf7b63772c6d71211e
Matthew Haas <matthew.haas1@gmail.com>
2022-04-04 15:12:45 -0500

    add population (# of samples) to x-axis

--- pop_gen_analyses/average_heterozygosity.R
+++ pop_gen_analyses/average_heterozygosity.R
@@ -33,8 +33,14 @@ boxplot(F ~ population_index, data = data,
 text(x = 1:length(populations),
      ## Move labels to just below bottom of chart.
      y = par("usr")[3] - 0.05,
-     ## Use names from the data list.
-     labels = populations,
+     ## specify population/sample names and how many individuals each population had
+     ## We could have used the `populations` list (and we initially did) but then wanted to add info about how many individuals each pop had.. so we must specify them literally
+     labels = c("14PD-C10 (2)", "14S*PM3/PBM-C3 (3)", "14S*PS (5)", "Zizania aquatica (50)", "Barron (25)", "Bass Lake (50)",
+                "Clearwater River (50)", "Dahler Lake (50)", "Decker Lake (50)", "FY-C20 (29)", "Garfield Lake (50)", "GPB/K2B-C2 (3)",
+                "GPN-1.20 (2)", "GPP-1.20 (6)", "Itasca-C12 (31)", "Itasca-C20 (11)", "K2B-C16 (4)", "K2EFBP-C1 (5)", "K2EF-C16 (20)", "KPVN-C4 (4)",
+                "KSVN-C4 (5)", "Mud Hen Lake (10)", "Necktie River (50)", "Ottertail River (50)", "Phantom Lake (20)", "Lake Plantagenet (50)",
+                "PLaR-C20 (4)", "PM3/3*PBM-C3 (6)", "PM3/7*K2EF (3)", "PM3E (17)", "Shell Lake (50)", "Upper Rice Lake (50)", "VE/2*14WS/*4K2EF (2)",
+                "VN/3*K2EF (4)"),
      ## Change the clipping region.
      xpd = NA,
      ## Rotate the labels by 35 degrees.
