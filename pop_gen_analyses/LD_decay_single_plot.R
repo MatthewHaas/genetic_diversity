f9ae95c26eeae24496ec2c087006f9f1715f8e89
Matthew Haas <matthew.haas1@gmail.com>
2022-04-05 09:48:16 -0500

    make side-by-side plots

--- pop_gen_analyses/LD_decay_single_plot.R
+++ pop_gen_analyses/LD_decay_single_plot.R
@@ -6,26 +6,42 @@ library(sommer) # the `sommer` package probably isn't necessary because we've al
 load("220401_LD_decay_separate_classes.Rdata")
 
 # Convert results to data.table foramt
-data <- as.data.table(cultivated_results_linked$all.LG)
+cm_data <- as.data.table(cultivated_results_linked$all.LG)
+ns_data <-as.data.table(natural_stands_results_linked$all.LG)
 
 # Filter for significant data (p < 0.001)
-# Note: right now, this only contains the results for the cultivated class of NWR. Ultimately, we will want to feature both classes in the same plot
-sig <- data[p < 0.001]
-sig <- sig[r2 < 1] # trying to improve loess curve
+cm_sig <- cm_data[p < 0.001]
+ns_sig <- ns_data[p < 0.001]
 
 # Apply loess function--takes a few minutes, so be patient :)
 # Also a first initial note: the loess curve is just a straight vertical line at x = 0
 # The predicted values are less than 1 (which makes sense given that we are working with R^2..but how to get it to go the length of the x-axis?)
-loess_values <- loess(sig$r2 ~ sig$d)
-xvals <- seq(min(sig$d), max(sig$d), (max(sig$d) - min(sig$d))/1e6)
+cm_loess_values <- loess(cm_sig$r2 ~ cm_sig$d)
+cm_xvals <- seq(min(cm_sig$d), max(cm_sig$d), (max(cm_sig$d) - min(cm_sig$d))/1e6)
+
+ns_loess_values <- loess(ns_sig$r2 ~ ns_sig$d)
+ns_xvals <- seq(min(ns_sig$d), max(ns_sig$d), (max(ns_sig$d) - min(ns_sig$d))/1e6)
 
 # other good green color: "#00a54c"
 
-pdf("out.pdf")
-sig[, plot(x = d, y = r2, xlab = "Distance",
+pdf("genome-wide_LD_separate_classes_side_by_side.pdf", height = 10, width = 20)
+layout(matrix(c(1,2), nrow = 1), widths = c(5,5))
+par(oma = c(1,1,1,1))
+par(mar = c(5,5,2,1))
+cm_sig[, plot(x = d, y = r2, xlab = "Distance (bp)",
 						  ylab = expression("R"^"2"),
+						  main = "Cultivated",
 						  pch = 16,
 						  col = "#a3aaad",
+						  cex.lab = 1.5,
 						  las = 1)]
-sig[, lines(xvals, predict(loess_values, xvals), col = "#235e39", lwd = 2)]
+cm_sig[, lines(cm_xvals, predict(cm_loess_values, cm_xvals), col = "#235e39", lwd = 2)]
+ns_sig[, plot(x = d, y = r2, xlab = "Distance (bp)",
+						  ylab = expression("R"^"2"),
+						  main = "Natural Stands",
+						  pch = 16,
+						  col = "#a3aaad",
+						  cex.lab = 1.5,
+						  las = 1)]
+ns_sig[, lines(ns_xvals, predict(ns_loess_values, ns_xvals), col = "#235e39", lwd = 2)]
 dev.off()
