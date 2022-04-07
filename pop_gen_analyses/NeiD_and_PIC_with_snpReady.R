0a95a872aabb7def777c381c885817023bcaacd0
Matthew Haas <matthew.haas1@gmail.com>
2022-04-07 14:40:53 -0500

    Fix issue with subgroups

--- pop_gen_analyses/NeiD_and_PIC_with_snpReady.R
+++ pop_gen_analyses/NeiD_and_PIC_with_snpReady.R
@@ -9,35 +9,30 @@ library(snpReady)
 # and a matrix of genotypes: columns are MARKERS and rows are INDIVIDUALS. Genotypes are in nucleotide format (e.g., "CC", "CG", "GG", etc)
 data <- fread("220406_snp_data_formatted_for_StAMPP.csv")
 
-# Create subgroups
-Bass_Lake <- data[Pop == "Bass Lake"]$Sample
-Clearwater_River <- data[Pop == "Clearwater River"]$Sample
-Dahler_Lake <- data[Pop == "Dahler Lake"]$Sample
-Decker_Lake <- data[Pop == "Decker Lake"]$Sample
-Garfield_Lake <- data[Pop == "Garfield Lake"]$Sample
-Mud_Hen_Lake <- data[Pop == "Mud Hen Lake"]$Sample
-Necktie_River <- data[Pop == "Necktie River"]$Sample
-Ottertail_River <- data[Pop == "Ottertail River"]$Sample
-Phantom_Lake <- data[Pop == "Phantom Lake"]$Sample
-Plantagenet <- data[Pop == "Plantagenet"]$Sample
-Shell_Lake <- data[Pop == "Shell Lake"]$Sample
-Upper_Rice_Lake <- data[Pop == "Upper Rice Lake"]$Sample
-Zizania_aquatica <- data[Pop == "Zizania_aquatica"]$Sample
-
 # Remove unnecessary columns
 data[, V1 := NULL]
-data[, [Pop := NULL]
+#data[, Pop := NULL]
 data[, Ploidy := NULL]
 data[, Format := NULL]
 
+# Make vector object of population names
+populations <- data$Pop
+
+# Remove white space from population names
+populations <- gsub(" ", "", populations)
+
 # Make vector object of sample names
-sample_names <- data$Sample
+#sample_names <- data$Sample
 
 # Now, we also need to remove the Sample column
 data[, Sample := NULL]
+# Remove Pop column name
+data[, Pop := NULL]
 
 # Convert data to matrix
 data_m <- as.matrix(data)
+
+rownames(data_m) <- populations
 
 # the `raw.data()` function converts the genotype data coded as AA, AB, BB back to 0, 1, 2
 geno.ready <- raw.data(data = data_m, frame = "wide", base = TRUE, sweep.sample = 0.5, call.rate = 0.95, maf = 0.10, imput = FALSE)
@@ -45,6 +40,9 @@ geno.ready <- raw.data(data = data_m, frame = "wide", base = TRUE, sweep.sample 
 # Convert "clean" data to new object
 M <- geno.ready$M.clean
 
+# Define subgroups
+subgroups <- as.matrix(c(rep("BassLake", 50), rep("ClearwaterRiver", 50), rep("DahlerLake", 50), rep("DeckerLake", 50), rep("GarfieldLake", 50), rep("MudHenLake", 10), rep("NecktieRiver", 50), rep("OttertailRiver", 50), rep("PhantomLake", 20), rep("Plantagenet", 50), rep("ShellLake", 50), rep("UpperRiceLake", 50)))
+
 # Do popgen analyses (they are all under this umbrella)
 # Before we can get the results we are looking for (namely, Nei's D and PIC) we need to define subgroups
-popgen_res <- popgen(M = M, subgroups = as.matrix(Bass_Lake, Clearwater_River, Dahler_Lake, Decker_Lake, Garfield_Lake, Mud_Hen_Lake, Necktie_River, Ottertail_River, Phantom_Lake, Plantagenet, Shell_Lake, Upper_Rice_Lake))
+popgen_res <- popgen(M = M, subgroups = subgroups)
