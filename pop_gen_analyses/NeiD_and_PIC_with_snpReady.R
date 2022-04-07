89717cc385a8d607d459d46e6e34a4321bfd2995
Matthew Haas <matthew.haas1@gmail.com>
2022-04-07 11:17:11 -0500

    Init commit

---
+++ pop_gen_analyses/NeiD_and_PIC_with_snpReady.R
@@ -0,0 +1,9 @@
+# purpose of this script is to calculate Nei's Diversity and Polymorphism Information Content with the R package snpReady
+library(data.table)
+library(impute) # probably not required to call this explicitly because it's a dependency of snpReady
+library(snpReady)
+
+# Read in data
+# For snpReady to work, 2 different files are needed: a table with 4 columns (sample, marler, allele.1, allele.2); and
+# and a matrix of genotypes: columns are MARKERS and rows are INDIVIDUALS. Genotypes are in nucleotide format (e.g., "CC", "CG", "GG", etc)
+data <- fread("220406_snp_data_formatted_for_StAMPP.csv")
