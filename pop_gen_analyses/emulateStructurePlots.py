51b385896fb2a6678a724c375e537caad5803a33
Matthew Haas <matthew.haas1@gmail.com>
2022-06-10 14:33:49 -0500

    remove refs to assignClusterMembership() function

--- pop_gen_analyses/emulateStructurePlots.py
+++ pop_gen_analyses/emulateStructurePlots.py
@@ -14,7 +14,6 @@ def makePlot(K):
         plt.figure(figsize = (20, 4.8)) # supposedly this is in inches...
         plt.xticks([]) # hide x-axis tick marks
         plt.yticks(fontsize = 18) # make tick marks and numbers larger (default = 12)
-        assignClusterMembership(K)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_2'], color = colorsAll[0], width = 1)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_1'], bottom = dfSorted['Cluster_2'], color = colorsAll[1], width = 1)
         plt.ylabel('Population membership probability', fontsize = 18)
@@ -28,7 +27,6 @@ def makePlot(K):
         plt.figure(figsize = (20, 4.8)) # supposedly this is in inches...
         plt.xticks([])
         plt.yticks(fontsize = 18) # make tick marks and numbers larger (default = 12)
-        assignClusterMembership(K)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_2'], color = colorsAll[0], width = 1)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_1'], bottom = dfSorted['Cluster_2'], color = colorsAll[1], width = 1)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_3'], bottom = dfSorted['Cluster_2'] + dfSorted['Cluster_1'], color = colorsAll[2], width = 1)
@@ -64,7 +62,6 @@ def makePlot(K):
         plt.figure(figsize = (20, 4.8)) # supposedly this is in inches...
         plt.xticks([])
         plt.yticks(fontsize = 18) # make tick marks and numbers larger (default = 12)
-        assignClusterMembership(K)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_2'], color = colorsAll[0], width = 1)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_1'], bottom = dfSorted['Cluster_2'], color = colorsAll[1], width = 1)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_3'], bottom = dfSorted['Cluster_2'] + dfSorted['Cluster_1'], color = colorsAll[2], width = 1)
@@ -87,7 +84,6 @@ def makePlot(K):
         plt.figure(figsize = (20, 4.8)) # supposedly this is in inches...
         plt.xticks([])
         plt.yticks(fontsize = 18) # make tick marks and numbers larger (default = 12)
-        assignClusterMembership(K)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_2'], color = colorsAll[0], width = 1)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_1'], bottom = dfSorted['Cluster_2'], color = colorsAll[1], width = 1)
         plt.bar(dfSorted['Sample_name'], dfSorted['Cluster_3'], bottom = dfSorted['Cluster_2'] + dfSorted['Cluster_1'], color = colorsAll[2], width = 1)
